d71974b74631bf6f15fdbc99ea8a0ff407fed1d7
GrallaX <alexey2141@gmail.com>
2020-12-17 20:30:15 +0200

    added 4.1

---
+++ hw-4/script/javascript.js
@@ -0,0 +1,129 @@
+console.log("Задание: 4.1");
+
+class Student {
+  static listOfStudents = [];
+  constructor(enrollee) {
+    this.id = Student.listOfStudents.length + 1;
+    this.name = enrollee.name;
+    this.surname = enrollee.surname;
+    this.ratingPoint = enrollee.ratingPoint;
+    this.schoolPoint = enrollee.schoolPoint;
+    this.isSelfPayment = false;
+    if (enrollee.ratingPoint >= 800) {
+      if (Student.listOfStudents.length < 5) {
+        this.isSelfPayment = true;
+      } else if (Student.listOfStudents.length >= 5) {
+        let sortedStudents = Student.listOfStudents.sort(function (min, max) {
+          if (min.ratingPoint == max.ratingPoint) {
+            return max.schoolPoint - min.schoolPoint;
+          } else {
+            return max.ratingPoint - min.ratingPoint;
+          }
+        });
+        if (enrollee.ratingPoint > sortedStudents[4].ratingPoint) {
+          this.isSelfPayment = true;
+          sortedStudents[4].isSelfPayment = false;
+        } else if (enrollee.ratingPoint == sortedStudents[4].ratingPoint) {
+          if (enrollee.schoolPoint > sortedStudents[4].schoolPoint) {
+            this.isSelfPayment = true;
+            sortedStudents[4].isSelfPayment = false;
+          }
+        }
+      }
+    }
+    Student.listOfStudents.push(this);
+    Student.listOfStudents.sort(function (min, max) {
+      if (min.ratingPoint == max.ratingPoint) {
+        return max.schoolPoint - min.schoolPoint;
+      } else {
+        return max.ratingPoint - min.ratingPoint;
+      }
+    });
+  }
+}
+
+const studentArr = [
+  {
+    name: "Сергей",
+    surname: "Войлов",
+    ratingPoint: 1000,
+    schoolPoint: 1000,
+    course: 2,
+  },
+  {
+    name: "Татьяна",
+    surname: "Коваленко",
+    ratingPoint: 880,
+    schoolPoint: 700,
+    course: 1,
+  },
+  {
+    name: "Анна",
+    surname: "Кугир",
+    ratingPoint: 1430,
+    schoolPoint: 1200,
+    course: 3,
+  },
+  {
+    name: "Станислав",
+    surname: "Щелоков",
+    ratingPoint: 1130,
+    schoolPoint: 1060,
+    course: 2,
+  },
+  {
+    name: "Денис",
+    surname: "Хрущ",
+    ratingPoint: 1000,
+    schoolPoint: 990,
+    course: 4,
+  },
+  {
+    name: "Татьяна",
+    surname: "Капустник",
+    ratingPoint: 650,
+    schoolPoint: 500,
+    course: 3,
+  },
+  {
+    name: "Максим",
+    surname: "Меженский",
+    ratingPoint: 990,
+    schoolPoint: 1100,
+    course: 1,
+  },
+  {
+    name: "Денис",
+    surname: "Марченко",
+    ratingPoint: 570,
+    schoolPoint: 1300,
+    course: 4,
+  },
+  {
+    name: "Антон",
+    surname: "Завадский",
+    ratingPoint: 1090,
+    schoolPoint: 1010,
+    course: 3,
+  },
+  {
+    name: "Игорь",
+    surname: "Куштым",
+    ratingPoint: 870,
+    schoolPoint: 790,
+    course: 1,
+  },
+  {
+    name: "Инна",
+    surname: "Скакунова",
+    ratingPoint: 1560,
+    schoolPoint: 200,
+    course: 2,
+  },
+];
+
+for (let pupil of studentArr) {
+  new Student(pupil);
+}
+
+console.log(Student.listOfStudents);
